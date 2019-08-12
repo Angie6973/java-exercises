9f6036aa21befe1a7c4a526bd38e2d199f918f5c
Angelika Stephanie Pierce <angiekinter@gmail.com>
2019-08-12 17:55:17 -0500

    Class 2 prep Exercise

---
+++ src/exercises/org/launchcode/java/studios/Area.java
@@ -0,0 +1,20 @@
+package exercises.org.launchcode.java.studios;
+
+import java.util.Scanner;
+
+public class Area {
+    public static void main (String[]args) {
+        Scanner in;
+
+        double radius, pi, area;
+        in = new Scanner(System.in);
+        System.out.println(" Enter the radius:");
+        radius= in.nextDouble();
+        pi = 3.14;
+        area = (radius * radius * pi);
+
+        System.out.println("The area of your circle is: " + area + " square units");
+
+
+    }
+}
